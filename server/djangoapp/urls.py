9936181df7545c97f0dc9be4dc620199b1f81601
Charmoni Pi <120690178+GitMon1@users.noreply.github.com>
2023-09-20 15:57:19 +0000

    1157

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,12 +15,19 @@ urlpatterns = [
     path('about/', views.about, name='about'),
 
     # path for contact us view
+    path('contact/', views.contact, name='contact'),
+
+    # path for profile
+    path('profile/', views.profile, name='profile'),
 
     # path for registration
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
+
 
     path(route='', view=views.get_dealerships, name='index'),
 
